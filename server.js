97e50a1229f8d08a008af03f755b53c69b942a8b
spff <spff@bitmessage.ch>
2019-01-02 00:27:49 +0800

    Add websocket to server side

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const serveStatic = require("serve-static")
+const SocketServer = require('ws').Server;
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
-app.listen(port);
+
+const app = express()
+  .use(serveStatic(path.join(__dirname, 'dist')))
+  .listen(port);
+
+const wss = new SocketServer({ server : app });
+console.log('Hi');
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
